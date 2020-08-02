49d7fac671228c255f163610b52fdd74eccb4b94
Adam Wilson <point5r@gmail.com>
2020-08-02 14:21:09 +0100

    feat: solve problem with python

--- 01-08-2020/python/PrefixMapSum.py
+++ 01-08-2020/python/PrefixMapSum.py
@@ -1,15 +1,20 @@
 class PrefixMapSum():
 
     def __init__(self):
-        self.map = {}
+        self.keymap = {}
 
     def insert(self, key, val):
+        self.keymap[key] = val
+
+    def sum(self, key):
         if len(key) > 3:
-            pref = key[3:]
+            pref = key[:3]
         else:
             pref = key
-        
-        return pref
 
-    def sum(self, pref):
-        pass        
+        valList = []
+        for k, v in self.keymap.items():
+            if k[:3] == pref:
+                valList.append(v)
+        
+        return sum(valList)
--- 01-08-2020/python/PrefixMapSum_test.py
+++ 01-08-2020/python/PrefixMapSum_test.py
@@ -1,12 +1,27 @@
 import PrefixMapSum as p
+import unittest
 
-mapsum = p.PrefixMapSum()
+class Test_PrefixMapSum(unittest.TestCase):
+    
+    def test_MapSum_Insert(self):
+        mapsum = p.PrefixMapSum()
+        want = {"columnar": 3}
+        mapsum.insert("columnar",3)
+        self.assertEqual(mapsum.keymap, want)
+        mapsum.insert("columnar",3)
+        self.assertEqual(mapsum.keymap, want)
 
-mapsum.insert("columnar", 3)
-assert mapsum.sum("col") == 3
+    def test_MapSum_Sum(self):
+        mapsum = p.PrefixMapSum()
+        mapsum.keymap = {"columnar": 3, "column": 2,}
+        want = 5
+        self.assertEqual(mapsum.sum("col"), want)
 
-mapsum.insert("columnar", 3)
-assert mapsum.sum("col") == 3
+# mapsum.insert("columnar", 3)
+# assert mapsum.sum("col") == 3
 
-mapsum.insert("column", 2)
-assert mapsum.sum("col") == 5
+# mapsum.insert("column", 2)
+# assert mapsum.sum("col") == 5
+
+if __name__ == '__main__':
+    unittest.main()
